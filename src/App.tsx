a550457a89f62ab240e8b14e24b08cce5f1940f2
Grayson Kornberg <grayson.kornberg@gmail.com>
2024-06-04 19:16:46 -0500

    setup app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App(): JSX.Element {
+    return (
+        <div>
+            <h1>Lyricipher</h1>
+            <p>
+                Lyricipher is a web app that lets you encrypt and decrypt text
+                using song lyrics.
+            </p>
+        </div>
+    )
+}
+
+export default App
